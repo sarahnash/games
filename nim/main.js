95de26d466dade7bf0e36e0856ce8ab52c445d6c
sarahnash <sarahnash1112@gmail.com>
2018-10-31 14:42:24 -0500

    best effort, it works

--- nim/main.js
+++ nim/main.js
@@ -1,13 +1,17 @@
 var content = document.getElementById('content');
 
-var nim = "Replace this with your own abstraction of Nim"
+content.innerHTML = renderGame();
 
-content.innerHTML = renderGame(nim);
+function init () {
+  // console.info('Initializing this stupid game')
+  content.innerHTML = renderGame();
+  document.getElementsByClassName('pebble')[0].setAttribute('style', 'background-color: orange;')
+  document.getElementsByClassName('pebble')[15].setAttribute('id', "last-pebble")
+  addEvents()
+  renderGame()
+}
 
-
-function renderGame(game) {
-    // Change this render function to use the "game" parameter
-
+function renderGame() {
     return `
         <div class="container d-flex flex-column justify-content-start align-items-center">
             <h4>There are 16 pebbles left</h4>
@@ -42,24 +46,18 @@ function renderGame(game) {
     `
 }
 
-function init () {
-  console.info('Initializing this stupid game')
-  document.getElementsByClassName('pebble')[0].setAttribute('style', 'background-color: orange;')
-  addEvents()
-  renderGame()
-}
 
 function addEvents () {
   targetEl = document.getElementsByClassName('btn')[0]
-  console.log(targetEl)
+  // console.log(targetEl)
   targetEl.addEventListener('click', takePebbles)
 }
 
 function takePebbles (evt) {
   // click the button to fill pebbles with takeInput
   var pebblesAmount = document.getElementById('takeInput').value
-  console.log(pebblesAmount)
-  console.info('Take pebbles')
+  // console.log(pebblesAmount)
+  // console.info('Take pebbles')
   colorPebbles()
 }
 
@@ -67,8 +65,7 @@ function colorPebbles () {
   var pebblesAttribute = document.getElementsByClassName('pebble')
   var pebblesAmount = document.getElementById('takeInput').value
   var count = pebblesAmount
-  console.info(count)
-
+ 
   for (i= 0; i < pebblesAttribute.length; i++) {
     if (pebblesAttribute[i].hasAttribute('style') && count !==0) {
       console.log('these are already colored')
@@ -77,19 +74,46 @@ function colorPebbles () {
       count -= 1
     }
   }
-  switchPlayer()
+  checkGame()
 }
 
-function switchPlayer () {
-  // need loop to see which player turn it is
-  console.log(document.getElementsByClassName('mt-5')[0]) 
-  if document.getElementsByClassName('mt-5')[0] === "It's player 1's turn!  How many pebblies will you take?"
-    // return document.getElementsByClassName('mt-5')[0].innerHTML = "It's player 2's turn!  How many pebblies will you take?"
 
+function switchPlayer () {
+  // console.info(document.getElementsByClassName('mt-5')[0].innerHTML)
+  if (document.getElementsByClassName('mt-5')[0].innerHTML === "It's player 1's turn! How many pebbles will you take?") {
+    document.getElementsByClassName('mt-5')[0].innerHTML = "It's player 2's turn! How many pebbles will you take?"
+  } else {
+    document.getElementsByClassName('mt-5')[0].innerHTML = "It's player 1's turn! How many pebbles will you take?"
+  }
 }
 
 function checkGame () {
-  // last pebble means game is over, the other player wins
+  var pebblesAttribute = document.getElementsByClassName('pebble')
+  if (pebblesAttribute[15].hasAttribute('style') && document.getElementsByClassName('mt-5')[0].innerHTML === "It's player 1's turn! How many pebbles will you take?") {
+    gameOver()
+  } else if (pebblesAttribute[15].hasAttribute('style') && document.getElementsByClassName('mt-5')[0].innerHTML === "It's player 2's turn! How many pebbles will you take?") {
+    gameOver()
+  } else {
+    console.info('the game continues')
+  }
+  switchPlayer()
 }
+
+function gameOver () {
+  if (document.getElementsByClassName('mt-5')[0].innerHTML === "It's player 1's turn! How many pebbles will you take?") {
+  content.innerHTML += `<div class="container d-flex flex-column justify-content-start align-items-center">
+                          <br>
+                          <h2>Player 2 Wins!</h2>
+                          <button onclick="init()">Play again</button>
+                      </div>`
+  } else {
+      content.innerHTML += `<div class="container d-flex flex-column justify-content-start align-items-center">
+                              <br>
+                              <h2>Player 1 Wins!</h2>
+                              <button onclick="init()">Play again</button>
+                          </div>`
+  }
+}
+
 
 document.addEventListener('DOMContentLoaded', init)
