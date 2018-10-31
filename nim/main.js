e82fce40a3a74d84d0a2d2c4d2f9e68b57e54629
sarahnash <sarahnash1112@gmail.com>
2018-10-30 21:38:30 -0500

    thank you Luis

--- nim/main.js
+++ nim/main.js
@@ -5,8 +5,6 @@ var nim = "Replace this with your own abstraction of Nim"
 content.innerHTML = renderGame(nim);
 
 
-
-
 function renderGame(game) {
     // Change this render function to use the "game" parameter
 
@@ -43,3 +41,55 @@ function renderGame(game) {
         </div>
     `
 }
+
+function init () {
+  console.info('Initializing this stupid game')
+  document.getElementsByClassName('pebble')[0].setAttribute('style', 'background-color: orange;')
+  addEvents()
+  renderGame()
+}
+
+function addEvents () {
+  targetEl = document.getElementsByClassName('btn')[0]
+  console.log(targetEl)
+  targetEl.addEventListener('click', takePebbles)
+}
+
+function takePebbles (evt) {
+  // click the button to fill pebbles with takeInput
+  var pebblesAmount = document.getElementById('takeInput').value
+  console.log(pebblesAmount)
+  console.info('Take pebbles')
+  colorPebbles()
+}
+
+function colorPebbles () {
+  var pebblesAttribute = document.getElementsByClassName('pebble')
+  var pebblesAmount = document.getElementById('takeInput').value
+  var count = pebblesAmount
+  console.info(count)
+
+  for (i= 0; i < pebblesAttribute.length; i++) {
+    if (pebblesAttribute[i].hasAttribute('style') && count !==0) {
+      console.log('these are already colored')
+    } else if (count !==0) {
+      pebblesAttribute[i].setAttribute('style', 'background-color: orange;')
+      count -= 1
+    }
+  }
+  switchPlayer()
+}
+
+function switchPlayer () {
+  // need loop to see which player turn it is
+  console.log(document.getElementsByClassName('mt-5')[0]) 
+  if document.getElementsByClassName('mt-5')[0] === "It's player 1's turn!  How many pebblies will you take?"
+    // return document.getElementsByClassName('mt-5')[0].innerHTML = "It's player 2's turn!  How many pebblies will you take?"
+
+}
+
+function checkGame () {
+  // last pebble means game is over, the other player wins
+}
+
+document.addEventListener('DOMContentLoaded', init)
